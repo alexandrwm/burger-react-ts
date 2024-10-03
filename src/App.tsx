e267f2a67575e944f6bb55e2cba9134e4576ebea
alexandrwm <alexandraciobanu2018@gmail.com>
2024-10-03 17:37:03 +0300

    add Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import BurgerCraftPage from './pages/BurgerCraftPage';
+import Acasa from './pages/Acasa';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Meniu from './pages/Meniu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+return (
+  <div className='w-screen h-screen overflow-hidden'>
+    <BrowserRouter>
+      <Routes>
+        <Route index element ={<Acasa/>}/>
+      </Routes>
+    </BrowserRouter>
+</div>
+)
 }
-
 export default App;
